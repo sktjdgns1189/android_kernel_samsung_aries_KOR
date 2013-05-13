be8e3de60e77280bb213b1e7d93d8b6da9b826e9
SungHun <sktjdgns1189@naver.com>
2013-05-13 18:09:24 +0900

    First Commit_Say JB Final

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 24 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#24 PREEMPT Mon May 13 17:51:27 KST 2013"
+#define LINUX_COMPILE_TIME "17:51:27"
+#define LINUX_COMPILE_BY "sunghun"
+#define LINUX_COMPILE_HOST "sunghun-System-Product-Name"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
